aa2fb8a0b415b427ab7068636f2733f8f6799dfb
Diego Bacuy <diegohd-20@outlook.com>
2023-03-08 01:22:19 -0500

    centralizacion de los widgets

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color primary = Colors.greenAccent;
+  static const Color primary = Colors.orangeAccent;
   static final ThemeData lightTheme = ThemeData.light().copyWith(
     //color primario
     primaryColor: Colors.indigo,
@@ -10,6 +10,11 @@ class AppTheme {
     appBarTheme: const AppBarTheme(
       color: primary,
       elevation: 0,
+    ),
+
+    //TextButton theme
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(foregroundColor: primary),
     ),
   );
 }
---
+++ lib/widgets/custom_card_type_1.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:fl_componentes/theme/app_theme.dart';
+
+class CustomCardType1 extends StatelessWidget {
+  const CustomCardType1({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          const ListTile(
+            leading: Icon(Icons.photo_camera_back, color: AppTheme.primary),
+            title: Text('Titulo'),
+            subtitle: Text(
+              'Aliquip minim adipisicing eiusmod minim in commodo exercitation pariatur sunt. Excepteur veniam quis sunt nisi id nostrud ipsum duis consectetur sit. Culpa consectetur ',
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(onPressed: () {}, child: const Text('Cancel')),
+                TextButton(onPressed: () {}, child: const Text('Ok')),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
